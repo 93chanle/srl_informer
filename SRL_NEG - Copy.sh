b20c40d872a067fcf0a82e052f56523813a17241
Chan Le <chan.le@sma.de>
2023-05-14 12:43:43 +0200

    new script

--- SRL_NEG - Copy.sh
+++ SRL_NEG - Copy.sh
@@ -13,3 +13,39 @@ python -u main_tuning.py --data SRL_NEG_12_16 --data_path SRL_NEG_12_16.csv --lo
 python -u main_tuning.py --data SRL_NEG_16_20 --data_path SRL_NEG_16_20.csv --loss linlin
 
 python -u main_tuning.py --data SRL_NEG_20_24 --data_path SRL_NEG_20_24.csv --loss linlin
+
+python -u main_tuning.py --data SRL_NEG_00_04 --data_path SRL_NEG_00_04.csv --loss linex
+
+python -u main_tuning.py --data SRL_NEG_04_08 --data_path SRL_NEG_04_08.csv --loss linex
+
+python -u main_tuning.py --data SRL_NEG_08_12 --data_path SRL_NEG_08_12.csv --loss linex
+
+python -u main_tuning.py --data SRL_NEG_12_16 --data_path SRL_NEG_12_16.csv --loss linex
+
+python -u main_tuning.py --data SRL_NEG_16_20 --data_path SRL_NEG_16_20.csv --loss linex
+
+python -u main_tuning.py --data SRL_NEG_20_24 --data_path SRL_NEG_20_24.csv --loss linex
+
+python -u main_tuning.py --data SRL_NEG_00_04 --data_path SRL_NEG_00_04.csv --loss w_rmse
+
+python -u main_tuning.py --data SRL_NEG_04_08 --data_path SRL_NEG_04_08.csv --loss w_rmse
+
+python -u main_tuning.py --data SRL_NEG_08_12 --data_path SRL_NEG_08_12.csv --loss w_rmse
+
+python -u main_tuning.py --data SRL_NEG_12_16 --data_path SRL_NEG_12_16.csv --loss w_rmse
+
+python -u main_tuning.py --data SRL_NEG_16_20 --data_path SRL_NEG_16_20.csv --loss w_rmse
+
+python -u main_tuning.py --data SRL_NEG_20_24 --data_path SRL_NEG_20_24.csv --loss w_rmse
+
+python -u main_tuning.py --data SRL_NEG_00_04 --data_path SRL_NEG_00_04.csv --loss rmse
+
+python -u main_tuning.py --data SRL_NEG_04_08 --data_path SRL_NEG_04_08.csv --loss rmse
+
+python -u main_tuning.py --data SRL_NEG_08_12 --data_path SRL_NEG_08_12.csv --loss rmse
+
+python -u main_tuning.py --data SRL_NEG_12_16 --data_path SRL_NEG_12_16.csv --loss rmse
+
+python -u main_tuning.py --data SRL_NEG_16_20 --data_path SRL_NEG_16_20.csv --loss rmse
+
+python -u main_tuning.py --data SRL_NEG_20_24 --data_path SRL_NEG_20_24.csv --loss rmse
